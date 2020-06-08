14065cd9056aa4f176dad00f5d921c54bd3845e8
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-06-08 19:10:28 +0900

    add database config

--- knexfile.js
+++ knexfile.js
@@ -16,33 +16,33 @@ module.exports = {
 
   staging: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
+      directory: "./db/migrations",
       tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
     },
   },
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
+      directory: "./db/migrations",
       tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
     },
   },
 };
